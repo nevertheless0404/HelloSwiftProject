fe4219e38d42927fb437f6a216d343a316c2a4f4
nevertheless0404 <nevertheless.hi@gmail.com>
2024-02-26 22:07:29 +0900

    따로 정리

--- HelloSwiftProject/study.swift
+++ HelloSwiftProject/study.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+
+// 문자열 리터럴
+// 토큰을 구별 할 수 있다면 탭이나, 앤터키를 쳐도 상관이 없음
+// 변수
+var name = "Swift"
+var thisYear = 2024
+var valid = true
+
+// variableName = initialValue
+name = "Steve"
+name = "유뇽"
+print(name)
+
+// let constantName = initialValue
+// 상수선언
+ let name = "Yuyeong"
+ name
+
+// 우선 상수로 저장 하고, 바꿀때 변수로 저장하기
+// 상수로 저장하면 안전 함.
