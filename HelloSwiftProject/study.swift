02d6d7397e1921326472e41436c8bbaa93d76d24
nevertheless0404 <nevertheless.hi@gmail.com>
2024-02-29 15:23:33 +0900

    Scope 정리

--- HelloSwiftProject/study.swift
+++ HelloSwiftProject/study.swift
@@ -16,14 +16,49 @@ var thisYear = 2024
 var valid = true
 
 // variableName = initialValue
-name = "Steve"
-name = "유뇽"
-print(name)
+//name = "Steve"
+//name = "유뇽"
+//print(name)
 
 // let constantName = initialValue
 // 상수선언
- let name = "Yuyeong"
- name
+// let name = "Yuyeong"
+// name
 
 // 우선 상수로 저장 하고, 바꿀때 변수로 저장하기
 // 상수로 저장하면 안전 함.
+
+// scope
+// #1 글로벌
+// 동일한 스코프에 접근할 수 있다.
+// 글로벌 스코프에서는 선언 순서에 상관없이 접근할 수 있다.
+let g1 = 123
+
+// 상위 스코프는 하위 스코프에 접근 할 수 없다.
+func doSoething() {
+    // #3
+    // 로컬 스코프에서 상위 스코프나 글로벌 스코프에 접근할 수 있다.
+    let local1 = 123
+    let g1 = 789
+    
+    print(g1)
+    
+    if true {
+        // #4
+        // 글로벌 스코프가 아니라면 이미 선언되어 있는 요소에만 접근 할 수 있다.
+        let local3 = 123
+    }
+    // #5
+    let local2 = 123
+    // print(local3)
+}
+
+// #2 글로벌
+let g2 = 456
+
+struct Scope {
+    var a = g1
+    
+}
+
+
